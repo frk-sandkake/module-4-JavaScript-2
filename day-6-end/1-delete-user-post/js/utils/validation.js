ad56247f853a5fc2e17ab468a9c2e9f39adef318
hesh ramsis <heshamelmasry1992@gmail.com>
2022-10-25 13:57:40 +0200

    changes from prettier

--- day-6-end/1-delete-user-post/js/utils/validation.js
+++ day-6-end/1-delete-user-post/js/utils/validation.js
@@ -1,22 +1,22 @@
 function validateEmail(email) {
-    const regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(stud.noroff.no|noroff.no)$/;
-    return email.match(regEx) ? true : false;
+  const regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@(stud.noroff.no|noroff.no)$/;
+  return email.match(regEx) ? true : false;
 }
 
 function validatePassword(password, confirmPassword) {
-    console.log(password)
-    console.log(confirmPassword)
-    if (!password) {
-        return false;
-    }
-    if (!confirmPassword) {
-        return false;
-    }
-    if (password !== confirmPassword) {
-        return false;
-    } else {
-        return true;
-    }
+  console.log(password);
+  console.log(confirmPassword);
+  if (!password) {
+    return false;
+  }
+  if (!confirmPassword) {
+    return false;
+  }
+  if (password !== confirmPassword) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-export {validateEmail, validatePassword}
+export { validateEmail, validatePassword };
