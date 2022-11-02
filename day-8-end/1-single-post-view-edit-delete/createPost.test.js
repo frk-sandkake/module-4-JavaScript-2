ad430ebf4043f584428e24948d4e49a5ceeb9d11
hesh ramsis <heshamelmasry1992@gmail.com>
2022-11-02 18:28:32 +0100

    improve the function and the test

--- day-8-end/1-single-post-view-edit-delete/createPost.test.js
+++ day-8-end/1-single-post-view-edit-delete/createPost.test.js
@@ -1,45 +1,37 @@
+// import the createPost Function
 import { createPost } from "./js/utils/createPost";
-// import the function I want to test
 
-// inputs for the function
-// 1. the token we pass to the function
+// INPUTS WE USE ON THE createPost FUNCTION
 
-const MOK_TOKEN = "gfnsidgfnsdouynfosvdf";
-
+// 1. The token we pass to the function, and it can be empty because we are mocking the API call
+const MOK_TOKEN = "";
+// 2. Data we pass to the function, the post title and the post description
 const MOK_POST_DATA = {
   title: "hello i am the testing title for the post",
   body: "hello i am the testing body of the post"
 };
-
-const MOK_API_URL = "helloIamSomeUrl";
-
+// 3. the API URL and it can be empty because we mock the API call
+const MOK_API_URL = "";
+// 4. mocked response of the API call after creating a post
 const MOK_RESPONSE = {
   id: 232
 };
 
 // handle fake jest MOCKED API CALL
-//
 global.fetch = jest.fn(() => Promise.resolve({
   json: () => Promise.resolve(MOK_RESPONSE)
 }));
 
-test("create post function", async () => {
-  console.log(MOK_TOKEN);
-  console.log(MOK_POST_DATA);
-  console.log(MOK_API_URL);
-
-  const response = await createPost(MOK_TOKEN, MOK_POST_DATA, MOK_API_URL);
-  console.log(response);
-  expect(response).toBeTruthy();
+// before the test, clear the cache
+beforeEach(() => {
+  fetch.mockClear();
 });
 
-
-
-
-
-
-
-
-
-
-
+it("create post function", async () => {
+  // use the createPost function with mocked data
+  const response = await createPost(MOK_TOKEN, MOK_POST_DATA, MOK_API_URL);
+  // assign the response id to a variable
+  const createdPostId = response.id;
+  // check if the returned post id is not undefined
+  expect(createdPostId).not.toBeUndefined();
+});
--- day-8-end/1-single-post-view-edit-delete/js/utils/createPost.js
+++ day-8-end/1-single-post-view-edit-delete/js/utils/createPost.js
@@ -1,25 +1,27 @@
-async function createPost(token, postData, apiPostUrl) {
+// createPost.js
+
+// This file has a function called createPost() which allow us to create a Post using the Noroff API.
+// This function require to have inputs token, postData, apiPostUrl (The user must be signedIn in order to have a valid TOKEN)
+
+async function createPost(token, postData, apiPostUrl) { //  parameters are token, the post data, the API end point URL to create post
   const response = await fetch(apiPostUrl, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}` // passing the token to the Authorization
     },
-    body: JSON.stringify(postData)
-  });
-  console.log("post creation response: ", response);
-  const jsonResponse = await response.json();
-  console.log("jsonResponse: ",jsonResponse)
-
-  if (jsonResponse.id) {
+    body: JSON.stringify(postData) // passing the new post data and converting it to String using JSON.stringify()
+  }); // API call to POST a new post data
+  const jsonResponse = await response.json(); // Converting the response to JSON
+  if (jsonResponse.id) { // Check if we have a post id after the post is created
+    // having the id of the created post means that the post is created
     console.log("CREATE POST SUCCEEDED!!  🥳 🤗🤗");
-    return true;
-    // location.href = "/index.html";
+    return jsonResponse; // return the post data
   } else {
-    return "Creating post failed";
+    return "Creating post failed"; // Error message returned if the API call failed
   }
 }
 
-export {
+export { // Exporting the function to use it anywhere in the project
   createPost
 };
